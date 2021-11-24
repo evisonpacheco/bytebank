2eb70f4cb710626f6e462e44784b13add379cb54
Ev1son <evison.pacheco21@gmail.com>
2021-11-24 19:54:26 -0300

    create: pasta newTransfer

---
+++ src/app/newTransfer/newTransfer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newTransfer',
+  templateUrl: './newTransfer.component.html',
+  styleUrls: ['./newTransfer.component.scss']
+})
+export class newTransferComponent {
+
+
+}
